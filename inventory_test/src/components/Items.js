4e1c3020190c939780c1f38a8ac4981f6e171b04
Karjen Wong <karjenwong@gmail.com>
2019-10-10 19:00:26 -0700

    added a modal to edit inventory

--- inventory_test/src/components/Items.js
+++ inventory_test/src/components/Items.js
@@ -1,8 +1,13 @@
-import React from "react";
-
+import React,{useState} from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 function Items({ data }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
   let list = data.map(item => {
     return (
       <Card style={{ width: "18rem" }} key={item.uid}>
@@ -13,17 +18,37 @@ function Items({ data }) {
           <Card.Text>Category: {item.category}</Card.Text>
           <Card.Text>Qty: {item.qty}</Card.Text>
           <Card.Text>Owner: {item.owner}</Card.Text>
-          <Button variant="primary">Edit</Button>
+          
+          <Button variant="primary" onClick={handleShow}>
+            Launch demo modal
+          </Button>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Woohoo, you're reading this text in a modal!
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleClose}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </Card.Body>
       </Card>
     );
   });
 
   return (
-    <>
+    <div className="inventory_container">
       <h1>All Inventory List</h1>
-      {list}
-    </>
+      <div className="inventoryList">{list}</div>
+    </div>
   );
 }
 
